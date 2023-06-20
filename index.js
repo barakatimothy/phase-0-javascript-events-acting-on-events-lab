1c567552a1f828923214ce44eef5aa157b7228b5
barakatimothy <timothybaraka39@gmail.com>
2023-06-20 05:35:18 -0400

    Complete!

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ document.addEventListener("keydown", function (e) {
       dodger.style.left = `${left - 1}px`;
     }
   }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
